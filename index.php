80257c1081d76fceb44df33b0a301399c6f80b51
dev3459 <test45@outlook.fr>
2021-01-29 14:13:04 +0100

    Exo 160

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
-
+$text = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum et non deleniti, vitae sint vel distinctio nobis dolor odio laborum ipsum cumque cupiditate maxime incidunt aliquam velit tenetur repellendus doloremque.";
+file_put_contents("lorem.txt", $text);
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
  * 4. Faites en sorte d'ajouter chaque chaîne de caractère de ce tableau au fichier créé dans la première partie ( point 2 )
@@ -15,20 +15,26 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
-
-
+$array = ["Salut", "Bonjour", "hey", "lets go", "Hello", "Hola", "Bnj", "Cc", "Slt", "Bye"];
+for($i = 0; $i < count($array); $i++){
+    $string .= $array[$i]." ";
+}
+file_put_contents("lorem.txt", $text.$string);
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
+$path = pathinfo("C:\Users\cleme\PhpstormProjects\Exo-160-PHP-Gestion-des-fichiers\index.php");
+echo $path["extension"]."<br>";
 
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
+if(!file_exists("toto.txt")){
+    echo "Le fichier n'est pas créer !";
+}
 
 /**
  * Super bonus.
@@ -36,3 +42,7 @@
  * Attention, il y a un piège avec les pointeurs, et une manière très simple de procéder... réfléchissez...
  */
 // TODO Votre code ici si vous faites le bonus.
+$file = file_get_contents("lorem.txt");
+$string = str_replace("a", "@", $file);
+echo $string;
+file_put_contents("lorem.txt", $string);
